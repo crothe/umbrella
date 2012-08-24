ac9c93b97c54c87f5fd037aceb474992f1b5bcd8
Chris Rothe <crothe@gmail.com>
2012-08-24 13:04:09 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umbrella::Application.config.secret_token = 'a3e24a61c2328a4f56f11065417d6ef4c6e627fd643e3fec2c866debe83f14a8e9886c325e307c40099e5c062f79cfda26ebbd59c5f9380f567a09dc0128f457'
